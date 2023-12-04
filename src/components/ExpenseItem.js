4524683da475b48acf24590eb187c56443f32806
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2023-12-04 12:50:23 +0530

    Passing Data using props and map method(t-9)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props){
+    // const expenseDate= new Date(2023, 2, 28);
+    // const expenseTitle= "Car Insurance";
+    // const expensePrice=294.4;
+    // const LocationOfExpenditure="Mumbai"
+
+
     return (<div className="expense-item">
-        <div>March 28th 2021</div> 
+        <div>{props.date.toISOString()}</div> 
         <div className="expense-item__description">
-            <h2>car insurance</h2>
-            <h2>Car ShowRoom</h2>
+            <h2>{props.title}</h2>
         </div>
-        <div className="expense-item__price">$120.70</div>
+        <div className='expense-item__location'>{props.location}</div>
+        <div className="expense-item__price">{props.amount}</div>
     </div>);
 }
 export default ExpenseItem;
