a8e384e277707d8dc8cbbd2a781718a01ec847f0
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2024-01-26 11:46:56 +0530

    Adding Event Listeners (t1)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from './ExpenseDate';
 import Card from "./Card";
 
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title)
+
+  const deleteButton = () =>{
+   setTitle('updated');
+    console.log(title)
+  }
     return (       
       <Card className="expense-item"> 
         <ExpenseDate date={props.date}/>
@@ -12,6 +18,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <h2>{props.locationOfExpenditure}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <button className='expense-item__delete' onClick={deleteButton}>Delete</button>
         </div>
       </Card>    
   );
